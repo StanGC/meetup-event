f4f6b4f23de1adbab2151b1068ffb2d4097b3cc7
Stan <solve153@gmail.com>
2017-08-10 16:35:22 +0800

    refactor test by One assertion per test

--- spec/controller/meetups_controller_spec.rb
+++ spec/controller/meetups_controller_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe MeetupsController, type: :controller do
 
   describe "GET index" do
-    it "assigns @meetups and render template" do
+    it "assigns @meetups" do
       meetup1 = create(:meetup)
       meetup2 = create(:meetup)
-      get:index
+      get :index
       expect(assigns[:meetups]).to eq([meetup1, meetup2])
+    end
+
+    it "render template" do
+      meetup1 = create(:meetup)
+      meetup2 = create(:meetup)
+
+      get :index
+
+      expect(response).to render_template("index")
     end
   end
 
