fc905641e90493decfcc15d3dd580c2082d55179
Stan <solve153@gmail.com>
2017-08-10 16:22:24 +0800

    gcmsg Implement controller test for meetups#show

--- spec/controller/meetups_controller_spec.rb
+++ spec/controller/meetups_controller_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe MeetupsController, type: :controller do
       expect(assigns[:meetups]).to eq([meetup1, meetup2])
     end
   end
+
+  describe "GET show" do
+    it "assigns @meetup" do
+      meetup = Meetup.create(title: "foo", description: "bar")
+      get :show, params: { id: meetup.id }
+      expect(assigns[:meetup]).to eq(meetup)
+    end
+
+    it "render template" do
+      meetup = Meetup.create(title: "foo", description: "bar")
+      get :show, params: { id: meetup.id }
+      expect(response).to render_template("show")
+    end
+  end
 end 
